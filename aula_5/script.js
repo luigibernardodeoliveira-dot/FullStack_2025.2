d4e4f35285a900c09577375db5104c8b595fcaf3
LUIGI OLIVEIRA <luigibernardodeoliveira@gmail.com>
2025-09-11 17:12:08 -0300

    atividade_avaliativa

---
+++ aula_5/script.js
@@ -0,0 +1,46 @@
+// window.alert("ola mundo ")
+console.log("ola mundo")
+nome = prompt("qual e o seu nome ?")
+console.log("ola" + nome )
+
+a = 1;
+b = 2;
+
+if(a < b){
+//
+}
+else if(b < a ) {
+//
+}
+else {
+//
+}
+
+i = 0;
+while(i < 10){
+    console.log(i);
+    //i++;
+    //i = i = 1;
+    i += 1 ; 
+
+}
+//for(let i=0; i <10;i++){
+ //   window.alert(i);
+//}
+//i = 0;
+//while(i<=100){
+ //   if(i%2 != 0 ){
+ //   console.log(i);
+//}
+  //  i++;
+
+ //}
+
+i = 0;
+while(i<100){
+    console.log(i);
+   1 += 2;
+}
+
+
+ 
