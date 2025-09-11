36f56124e4c9b63cb8af0033ec6086e499ee745d
LUIGI OLIVEIRA <luigibernardodeoliveira@gmail.com>
2025-09-11 11:09:36 -0300

    trabalhos_da_semana

---
+++ index.html/Untitled-1.py
@@ -0,0 +1,7 @@
+ano_carro= ("digite o ano do cara")
+ano_atual= ("digite o ano atual")
+idade= ano_carro-ano_atual
+print ("o resuldado da conta e:",idade)
+if idade <=3:
+    print("carro novoo")
+else: print("carro velho")
---
+++ python/primeiro.py
@@ -0,0 +1,4 @@
+velocidade=int(input("informe a velocidade"))
+if velocidade>120: 
+    print("esta acima do limite")
+    
