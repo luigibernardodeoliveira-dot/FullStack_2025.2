d2c486c5b822c49e3ede3fb38e8e0c6dc2686dd3
LUIGI OLIVEIRA <luigibernardodeoliveira@gmail.com>
2025-09-21 00:14:41 -0300

    criei um game e organizei o repositorio

---
+++ projeto_principal/adivinhação/guess.js
@@ -0,0 +1,27 @@
+let num_adivinha = Math.random() * 100;
+let valor_num = Math.floor(num_adivinha);
+
+function Guess() {
+  let chute = parseInt(document.getElementById("dgt_num").value);
+
+  console.log(`nmr_chute: ${chute}`);
+  console.log(`nmr_sorteado: ${valor_num}`);
+
+
+  if (chute === valor_num) {
+    document.getElementById("resultado").style.setProperty("background-color" , "green");
+    document.getElementById("resultado").innerHTML = "Parabéns! Você acertou o número!";
+  }
+
+
+  else if (chute < valor_num) {
+    document.getElementById("tentativa_pqn").style.setProperty("background-color", "red");
+    document.getElementById("tentativa_pqn").innerHTML += `${chute} , `;
+  }
+
+  
+  else if (chute > valor_num) {
+    document.getElementById("tentativa_grd").style.setProperty("background-color", "red");
+    document.getElementById("tentativa_grd").innerHTML += `${chute} ,`;
+  }
+}
--- python/primeiro.py
+++
@@ -1,4 +0,0 @@
-velocidade=int(input("informe a velocidade"))
-if velocidade>120: 
-    print("esta acima do limite")
-    
